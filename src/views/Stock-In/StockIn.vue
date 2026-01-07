1366243a96c3bd3f33a726fc22d4bfd705688fa9
Samnangnouy <samnangnouy200@gmail.com>
2026-01-07 14:16:56 +0700

    Update card

--- src/views/Stock-In/StockIn.vue
+++ src/views/Stock-In/StockIn.vue
@@ -121,7 +121,7 @@ const completeStockIn = async () => {
           <!-- Product cards -->
           <div class="">
             <div
-              class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6"
+              class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
             >
               <ProductCard
                 v-for="product in products"
